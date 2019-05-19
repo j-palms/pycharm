68329e1215a7193ddf9b1e12b0317b7463ad66f1
j-palms <rasleul@gmail.com>
2019-05-19 12:05:15 +0200

    Initial commit

---
+++ function homework.py
@@ -0,0 +1,15 @@
+def up_low(string):
+    for i in string:
+        u = 0
+        l = 0
+        if i == i.upper:
+            u += 1
+        else:
+            l += 1
+    print ("The string contains:{} upper case and {} lower case letters!".format(u,l))
+    return [u,l]
+
+a = up_low("Hallo Ich Kann Zaehlen")
+print (a)
+print (10)
+        `
